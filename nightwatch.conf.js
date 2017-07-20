1be353a81f8050b9085e98d8896bacfdf9005394
petamoriken <moriken@kimamass.com>
2017-07-21 03:14:10 +0900

    support for pull request in TravisCI

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,9 +1,9 @@
 const settings = require("./nightwatch.json");
 
-const { TRAVIS_JOB_NUMBER, TRAVIS_BRANCH } = process.env;
+const { TRAVIS_JOB_NUMBER, TRAVIS_BRANCH, TRAVIS_PULL_REQUEST } = process.env;
 if(TRAVIS_JOB_NUMBER) {
     const desiredCapabilities = settings.test_settings.default.desiredCapabilities;
-    if(TRAVIS_BRANCH === "master") {
+    if(TRAVIS_BRANCH === "master" && TRAVIS_PULL_REQUEST === "false") {
         desiredCapabilities.build = `build-${ TRAVIS_JOB_NUMBER }`;
     }
     desiredCapabilities["tunnel-identifier"] = TRAVIS_JOB_NUMBER;
