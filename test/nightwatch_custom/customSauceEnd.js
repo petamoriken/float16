e7da15489a879e5a81fdd1b4549bf9e0f5c0bb6c
petamoriken <moriken@kimamass.com>
2019-05-26 21:06:23 +0900

    Fix customSauceEnd & Tweak

--- test/nightwatch_custom/customSauceEnd.js
+++ test/nightwatch_custom/customSauceEnd.js
@@ -1,18 +1,16 @@
 "use strict";
 
-const SauceLabs = require("saucelabs");
+const SauceLabs = require("saucelabs").default;
 
 // environment
 const { SAUCE_USERNAME: user, SAUCE_ACCESS_KEY: key } = process.env;
 
 exports.command = async function() {
-    const { sessionId, currentTest: { name } } = this;
+    const { sessionId, currentTest: { name, results: { failed } } } = this;
+    const passed = failed === 0;
 
     const saucelabs = new SauceLabs({ user, key });
-    await saucelabs.updateJob(user, sessionId, {
-        name,
-        passed: this.currentTest.results.failed === 0,
-    });
+    await saucelabs.updateJob(user, sessionId, { name, passed });
 
     this.end();
 };
