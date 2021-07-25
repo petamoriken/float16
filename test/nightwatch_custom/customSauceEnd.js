58a6a8b6d9be16cf87f672b6bc83222b95481405
Kenta Moriuchi <moriken@kimamass.com>
2021-07-26 02:20:56 +0900

    Update dependencies (#434)
    * Update dependencies
    * Fix E2E test

--- test/nightwatch_custom/customSauceEnd.js
+++ test/nightwatch_custom/customSauceEnd.js
@@ -5,7 +5,7 @@ const SauceLabs = require("saucelabs").default;
 // environment
 const { SAUCE_USERNAME: user, SAUCE_ACCESS_KEY: key } = process.env;
 
-exports.command = async function() {
+exports.command = async function () {
     const { sessionId, currentTest: { name, results: { failed } } } = this;
     const passed = failed === 0;
 
