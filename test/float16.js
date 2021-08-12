c4ed395287e14640ec125e93db080b0df27e11a3
petamoriken <petamoriken@users.noreply.github.com>
2021-08-12 00:28:08 +0000

    deploy: 7a286614a8d01e5fb34d8650e325042623748f48

--- test/float16.js
+++ test/float16.js
@@ -1,5 +1,5 @@
 /**
- * @petamoriken/float16 v3.2.3-30-g4535c8b | MIT License - https://git.io/float16
+ * @petamoriken/float16 v3.2.3-31-g7a28661 | MIT License - https://git.io/float16
  *
  * @license
  * lodash-es v4.17.21 | MIT License - https://lodash.com/custom-builds
