9b01d5ea6a139789e39fb2deb5aec52431063d7a
Kenta Moriuchi <moriken@kimamass.com>
2021-08-24 21:59:26 +0900

    Update to support types for Deno

--- index.mjs
+++ index.mjs
@@ -1,3 +1,5 @@
+/// <reference types="./index.d.ts" />
+
 export { hfround } from "./src/hfround.mjs";
 export { Float16Array, isFloat16Array } from "./src/Float16Array.mjs";
 export { getFloat16, setFloat16 } from "./src/DataView.mjs";
