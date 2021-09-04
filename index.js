0b55cce481013ffa894e65ccca8161450c2d01aa
Kenta Moriuchi <moriken@kimamass.com>
2021-09-04 10:08:57 +0900

    Add assumptions field to Babel config to tweak the CommonJS conversion

--- index.js
+++ index.js
@@ -8,33 +8,8 @@ const { setFloat16, getFloat16 } = require("./lib/DataView.js");
 const { Float16Array, isFloat16Array } = require("./lib/Float16Array.js");
 const { hfround } = require("./lib/hfround.js");
 
-Object.defineProperty(exports, "hfround", {
-  enumerable: true,
-  get: function () {
-    return hfround;
-  },
-});
-Object.defineProperty(exports, "Float16Array", {
-  enumerable: true,
-  get: function () {
-    return Float16Array;
-  },
-});
-Object.defineProperty(exports, "isFloat16Array", {
-  enumerable: true,
-  get: function () {
-    return isFloat16Array;
-  },
-});
-Object.defineProperty(exports, "getFloat16", {
-  enumerable: true,
-  get: function () {
-    return getFloat16;
-  },
-});
-Object.defineProperty(exports, "setFloat16", {
-  enumerable: true,
-  get: function () {
-    return setFloat16;
-  },
-});
+exports.setFloat16 = setFloat16;
+exports.getFloat16 = getFloat16;
+exports.Float16Array = Float16Array;
+exports.isFloat16Array = isFloat16Array;
+exports.hfround = hfround;
