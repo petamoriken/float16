6f98d877a8a74b0570387ff7557c49975f3fb074
Kenta Moriuchi <moriken@kimamass.com>
2021-09-06 17:26:13 +0900

    Wait .suite elements

--- test/browser/index.js
+++ test/browser/index.js
@@ -12,7 +12,7 @@ const { TARGET_URL } = process.env;
 module.exports = {
   async ["Browser Test"](client) {
     const elements = await client.url(TARGET_URL || "http://127.0.0.1:8000/power.html")
-      .waitForElementPresent("#mocha-report")
+      .waitForElementPresent("#mocha-report .suite:nth-of-type(4)")
       .timeoutsImplicitWait(3000)
       .findElements("#mocha-report .test .error");
 
