4be42bdaaf993c3cb84f08d09d882f596aa36dc5
Kenta Moriuchi <moriken@kimamass.com>
2021-10-11 10:00:49 +0900

    Update custom inspect for deno

--- inspect/deno.ts
+++ inspect/deno.ts
@@ -1,12 +1,13 @@
+import type { Float16Array } from "../mod.ts";
+
 /**
- * @todo Current custom inspector cannot access options. See [enhance custom inspect API proposal](https://github.com/denoland/deno/issues/8099#issuecomment-757434256)
  * @example
  * ```
  * // deno-lint-ignore no-explicit-any
  * (Float16Array.prototype as any)[Symbol.for("Deno.customInspect")] = customInspect;
  * ```
  */
-export function customInspect(inspect: typeof Deno.inspect): string {
+ export function customInspect(this: Float16Array, inspect: typeof Deno.inspect, options: Deno.InspectOptions): string {
   const length = this.length;
 
   const array = [];
@@ -14,5 +15,5 @@ export function customInspect(inspect: typeof Deno.inspect): string {
     array[i] = this[i];
   }
 
-  return `Float16Array(${length}) ${inspect(array)}`;
+  return `Float16Array(${length}) ${inspect(array, options)}`;
 }
