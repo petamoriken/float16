7f545e3ee39fdaa46af2d1505a7897a9d0b60a95
petamoriken <petamoriken@users.noreply.github.com>
2022-06-22 16:37:46 +0000

    deploy: f00cb9e1af1c4048b8976d5f5db47ec9873670c1

--- test/float16.js
+++ test/float16.js
@@ -1,4 +1,4 @@
-/*! @petamoriken/float16 v3.6.3-80-g54cf439 | MIT License - https://github.com/petamoriken/float16 */
+/*! @petamoriken/float16 v3.6.3-81-gf00cb9e | MIT License - https://github.com/petamoriken/float16 */
 
 const float16 = (function (exports) {
   'use strict';
