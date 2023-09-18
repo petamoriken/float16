1d9877013fa48807907073582e2fdd4abfc85f04
Kenta Moriuchi <moriken@kimamass.com>
2023-09-18 22:24:06 +0900

    fix rounding to float16 (#1054)

--- src/_util/converter.mjs
+++ src/_util/converter.mjs
@@ -1,40 +1,114 @@
 import {
+  EPSILON,
   MathAbs,
-  MathFloor,
-  MathLog2,
-  MathPow,
-  MathSign,
-  MathTrunc,
   NativeArrayBuffer,
   NativeFloat32Array,
   NativeUint16Array,
   NativeUint32Array,
+  NativeUint8Array,
   NumberIsFinite,
   NumberIsNaN,
-  ObjectIs,
 } from "./primordials.mjs";
 
-const mantissaBitLength = 10;
-const mantissaMask = 0x3ff;
-const exponentMax = 31;
-const exponentBias = 15;
-
-// base algorithm: https://github.com/feross/ieee754
-// BSD-3-Clause License. Feross Aboukhadijeh <https://feross.org/opensource>
+const INVERSE_OF_EPSILON = 1 / EPSILON;
 
 /**
- * round a number to nearest integer value;
- * if the number is halfway, it is rounded to the nearest even number
- * @param {number} num - double float
+ * rounds to the nearest value;
+ * if the number falls midway, it is rounded to the nearest value with an even least significant digit
+ * @param {number} num
+ * @returns {number}
+ */
+function roundTiesToEven(num) {
+  return (num + INVERSE_OF_EPSILON) - INVERSE_OF_EPSILON;
+}
+
+const FLOAT16_MIN_VALUE = 6.103515625e-05;
+const FLOAT16_MAX_VALUE = 65504;
+const FLOAT16_EPSILON = 0.0009765625;
+
+const FLOAT16_EPSILON_MULTIPLIED_BY_FLOAT16_MIN_VALUE = FLOAT16_EPSILON * FLOAT16_MIN_VALUE;
+const FLOAT16_EPSILON_DEVIDED_BY_EPSILON = FLOAT16_EPSILON * INVERSE_OF_EPSILON;
+
+/**
+ * round a number to a half float number
+ * @param {unknown} num - double float
  * @returns {number} half float number bits
  */
-function roundToEven(num) {
-  const truncated = MathTrunc(num);
-  const delta = MathAbs(num - truncated);
-  if (delta > 0.5 || delta === 0.5 && truncated % 2 !== 0) {
-    return truncated + MathSign(num);
+export function roundToFloat16(num) {
+  const number = +num;
+
+  // NaN, Infinity, -Infinity, 0, -0
+  if (!NumberIsFinite(number) || number === 0) {
+    return number;
   }
-  return truncated;
+
+  // finite except 0, -0
+  const sign = number > 0 ? 1 : -1;
+  const absolute = MathAbs(number);
+
+  // small number
+  if (absolute < FLOAT16_MIN_VALUE) {
+    return sign * roundTiesToEven(absolute / FLOAT16_EPSILON_MULTIPLIED_BY_FLOAT16_MIN_VALUE) * FLOAT16_EPSILON_MULTIPLIED_BY_FLOAT16_MIN_VALUE;
+  }
+
+  const temp = (1 + FLOAT16_EPSILON_DEVIDED_BY_EPSILON) * absolute;
+  const result = temp - (temp - absolute);
+
+  // large number
+  if (result > FLOAT16_MAX_VALUE || NumberIsNaN(result)) {
+    return sign * Infinity;
+  }
+
+  return sign * result;
+}
+
+// base algorithm: http://fox-toolkit.org/ftp/fasthalffloatconversion.pdf
+
+const buffer = new NativeArrayBuffer(4);
+const floatView = new NativeFloat32Array(buffer);
+const uint32View = new NativeUint32Array(buffer);
+
+const baseTable = new NativeUint16Array(512);
+const shiftTable = new NativeUint8Array(512);
+
+for (let i = 0; i < 256; ++i) {
+  const e = i - 127;
+
+  // very small number (0, -0)
+  if (e < -27) {
+    baseTable[i]         = 0x0000;
+    baseTable[i | 0x100] = 0x8000;
+    shiftTable[i]         = 24;
+    shiftTable[i | 0x100] = 24;
+
+  // small number (denorm)
+  } else if (e < -14) {
+    baseTable[i]         =  0x0400 >> (-e - 14);
+    baseTable[i | 0x100] = (0x0400 >> (-e - 14)) | 0x8000;
+    shiftTable[i]         = -e - 1;
+    shiftTable[i | 0x100] = -e - 1;
+
+  // normal number
+  } else if (e <= 15) {
+    baseTable[i]         =  (e + 15) << 10;
+    baseTable[i | 0x100] = ((e + 15) << 10) | 0x8000;
+    shiftTable[i]         = 13;
+    shiftTable[i | 0x100] = 13;
+
+  // large number (Infinity, -Infinity)
+  } else if (e < 128) {
+    baseTable[i]         = 0x7c00;
+    baseTable[i | 0x100] = 0xfc00;
+    shiftTable[i]         = 24;
+    shiftTable[i | 0x100] = 24;
+
+  // stay (NaN, Infinity, -Infinity)
+  } else {
+    baseTable[i]         = 0x7c00;
+    baseTable[i | 0x100] = 0xfc00;
+    shiftTable[i]         = 13;
+    shiftTable[i | 0x100] = 13;
+  }
 }
 
 /**
@@ -43,54 +117,11 @@ function roundToEven(num) {
  * @returns {number} half float number bits
  */
 export function roundToFloat16Bits(num) {
-  const absolute = MathAbs(/** @type {number} */ (num));
-
-  const s = /** @type {number} */ (num) < 0 || ObjectIs(num, -0) ? 1 : 0;
-  let m, e;
-
-  // NaN, Infinity, -Infinity
-  if (!NumberIsFinite(absolute)) {
-    // quiet NaN
-    m = NumberIsNaN(absolute) ? 0x200 : 0;
-    e = exponentMax;
-
-  // finite
-  } else {
-    let rawE = MathFloor(MathLog2(absolute));
-    let c = MathPow(2, -rawE);
-    if (absolute * c < 1) {
-      --rawE;
-      c *= 2;
-    } else if (absolute * c >= 2) {
-      ++rawE;
-      c /= 2;
-    }
-
-    // Infinity, -Infinity
-    if (rawE + exponentBias >= exponentMax) {
-      m = 0;
-      e = exponentMax;
-
-    // normal number
-    } else if (rawE + exponentBias >= 1) {
-      m = roundToEven(((absolute * c) - 1) * 0x400);
-      e = rawE + exponentBias;
-
-    // subnormal number, 0, -0
-    } else {
-      m = roundToEven(absolute * 0x1000000);
-      e = 0;
-    }
-  }
-
-  return s << 15 | e << mantissaBitLength | m;
+  floatView[0] = roundToFloat16(num);
+  const f = uint32View[0];
+  const e = (f >> 23) & 0x1ff;
+  return baseTable[e] + ((f & 0x007fffff) >> shiftTable[e]);
 }
-
-// base algorithm: http://fox-toolkit.org/ftp/fasthalffloatconversion.pdf
-
-const buffer = new NativeArrayBuffer(4);
-const floatView = new NativeFloat32Array(buffer);
-const uint32View = new NativeUint32Array(buffer);
 
 const mantissaTable = new NativeUint32Array(2048);
 for (let i = 1; i < 1024; ++i) {
@@ -136,7 +167,7 @@ for (let i = 1; i < 64; ++i) {
  * @returns {number} double float
  */
 export function convertToNumber(float16bits) {
-  const i = float16bits >> mantissaBitLength;
-  uint32View[0] = mantissaTable[offsetTable[i] + (float16bits & mantissaMask)] + exponentTable[i];
+  const i = float16bits >> 10;
+  uint32View[0] = mantissaTable[offsetTable[i] + (float16bits & 0x3ff)] + exponentTable[i];
   return floatView[0];
 }
